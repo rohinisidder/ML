24fa6322d8209212d7c7de1bbdba864f50951e52
rohinisidder <rohini252003@gmail.com>
2024-07-02 23:39:59 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,106 +1,75 @@
 import csv
 import random
 import math
+def main():
+    filename = 'diabetes.csv'
+    dataset = load_csv(filename)
+    split_ratio = 0.87
+    training_set,test_set = split_dataset(dataset,split_ratio)
+    separated = separated_by_class(training_set)
+    summaries = {class_value:summarize(row) for class_value,row in separated.items()}
+    predictions = get_predictions(summaries,test_set)
+    accuracy = get_accuracy(test_set,predictions)
+    print(accuracy)
 
-def loadCsv(filename):
-    lines = csv.reader(open(filename, "r"))
-    dataset = list(lines)
-    for i in range(len(dataset)):
-        dataset[i] = [float(x) for x in dataset[i]]
+def load_csv(filename):
+    with open(filename,'r') as file:
+        lines = csv.reader(file)
+        dataset = [list(map(float,row)) for row in lines]
     return dataset
 
-def splitDataset(dataset, splitRatio):
-    trainSize = int(len(dataset) * splitRatio)
-    trainSet = []
-    copy = list(dataset)
-    while len(trainSet) < trainSize:
-        index = random.randrange(len(copy))
-        trainSet.append(copy.pop(index))
-    return [trainSet, copy]
+def split_dataset(dataset,split_ratio):
+    train_size = int(len(dataset)*split_ratio)
+    train_set = random.sample(dataset,train_size)
+    test_set = [row for row in dataset if row not in train_set]
+    return train_set,test_set
 
-def separateByClass(dataset):
+def separated_by_class(dataset):
     separated = {}
-    for i in range(len(dataset)):
-        vector = dataset[i]
-        if (vector[-1] not in separated):
-            separated[vector[-1]] = []
-        separated[vector[-1]].append(vector)
+    for row in dataset:
+        class_value = row[-1]
+        if class_value not in separated:
+            separated[class_value] = []
+        separated[class_value].append(row)
     return separated
 
+def summarize(dataset):
+    summaries = [(mean(col),stdev(col)) for col in zip(*dataset)]
+    return summaries[:-1]
+
 def mean(numbers):
-    return sum(numbers)/float(len(numbers))
+    mean = sum(numbers)/float(len(numbers))
+    return mean
 
 def stdev(numbers):
     avg = mean(numbers)
-    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+    variance = sum((x-avg)**2/float(len(numbers)-1) for x in numbers)
     return math.sqrt(variance)
 
-def summarize(dataset):
-    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-    del summaries[-1]
-    return summaries
+def get_predictions(summaries,test_set):
+    return [predictions(summaries,row) for row in test_set]
 
-def summarizeByClass(dataset):
-    separated = separateByClass(dataset)
-    summaries = {}
-    for classValue, instances in separated.items():
-        summaries[classValue] = summarize(instances)
-    return summaries
+def predictions(summaries,input_vector):
+    probabilities = calculate_class_probabilities(summaries,input_vector)
+    return max(probabilities,key=probabilities.get)
 
-def calculateProbability(x, mean, stdev):
-    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
-
-def calculateClassProbabilities(summaries, inputVector):
+def calculate_class_probabilities(summaries,input_vector):
     probabilities = {}
-    for classValue, classSummaries in summaries.items():
-        probabilities[classValue] = 1
-        for i in range(len(classSummaries)):
-            mean, stdev = classSummaries[i]
-            x = inputVector[i]
-            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    for class_value,class_summaries in summaries.items():
+        probabilities[class_value] = 1
+        for i in range(len(class_summaries)):
+            mean,stdev = class_summaries[i]
+            x = input_vector[i]
+            probabilities[class_value]*=calculate_probability(mean,stdev,x)
     return probabilities
 
-def predict(summaries, inputVector):
-    probabilities = calculateClassProbabilities(summaries, inputVector)
-    bestLabel, bestProb = None, -1
-    for classValue, probability in probabilities.items():
-        if bestLabel is None or probability > bestProb:
-            bestProb = probability
-            bestLabel = classValue
-    return bestLabel
+def calculate_probability(mean,stdev,x):
+    exponent = math.exp(-((x-mean)**2/(2*stdev**2)))
+    return (1/(math.sqrt(2*math.pi)*stdev))*exponent
 
-def getPredictions(summaries, testSet):
-    predictions = []
-    for i in range(len(testSet)):
-        result = predict(summaries, testSet[i])
-        predictions.append(result)
-    return predictions
-
-def getAccuracy(testSet, predictions):
-    correct = 0
-    for i in range(len(testSet)):
-        if testSet[i][-1] == predictions[i]:
-            correct += 1
-    return (correct/float(len(testSet))) * 100.0
-
-def main():
-    filename = 'diabetes.csv'
-    splitRatio = 0.87
-    dataset = loadCsv(filename)
-    trainingSet, testSet = splitDataset(dataset, splitRatio)
-    
-    print("----------------------------------Output of naïve Bayesian classifier\n")
-    print('Splitting {} rows into training={} and testing={} rows'.format(len(dataset), len(trainingSet), len(testSet)))
-    
-    # prepare model
-    summaries = summarizeByClass(trainingSet)
-    
-    # test model
-    predictions = getPredictions(summaries, testSet)
-    accuracy = getAccuracy(testSet, predictions)
-    
-    print('Classification Accuracy: {}%'.format(accuracy))
-    print("-----------------------------------------------------")
+def get_accuracy(test_set,predictions):
+    correct = sum(1 for i in range(len(test_set)) if test_set[i][-1]==predictions[i])
+    return (correct/float(len(test_set)))*100.0
 
 main()
+    
